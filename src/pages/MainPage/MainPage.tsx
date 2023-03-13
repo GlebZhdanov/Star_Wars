46b02774ed10787237ca702e19e907569f2e2414
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2023-03-14 00:36:14 +0300

    add fonts

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import classes from './MainPage.module.scss'
+import BannerImage from 'shared/accets/image/banner.svg';
+
 
 const MainPage = () => {
   return (
     <div className={classes.Main}>
-      MAIN PAGE
+      <div>
+        <h1 className={classes.title}>Find all your favorite character</h1>
+        <h1 className={classes.subtitle}>Find all your favorite character</h1>
+        <p>You can find out all the information about your favorite characters</p>
+        <button>
+          See more...
+        </button>
+      </div>
+      <BannerImage />
     </div>
   );
 };
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Sidebar} from "../Sidebar/ui/Sidebar";
 import classes from './Header.module.scss'
+import HeaderIcon from 'shared/accets/image/icon-header.svg';
 
 export const Header = () => {
   return (
     <div className={classes.Header}>
-      <p>Картинка</p>
+      <HeaderIcon />
       <div className={classes.content_link}>
         <Sidebar />
       </div>
